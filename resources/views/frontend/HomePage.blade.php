22676227b810160a911935977e5a7249a20757b8
ENG mohammed <mmsh7398@gmail.com>
2020-09-25 00:29:21 +0300

    edit

--- resources/views/frontend/HomePage.blade.php
+++ resources/views/frontend/HomePage.blade.php
@@ -16,7 +16,8 @@
         <ul>
             <li><a href={{route('sharing.create')}}>مشاركة</a></li>
             <li><a href={{route('sharing.show')}}>معرض المشاركات</a></li>
-            <li><a href={{route('download.home')}}>13 في قلب واحد</a></li>
+            <li style="direction: rtl"> <a href={{route('download.home')}}>13 في قلب واحد</a></li>
+
 
         </ul>
     </nav>
