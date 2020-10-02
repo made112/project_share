256e91ab810830ed8f4f5f2de1bf56d262e2e996
ENG mohammed <mmsh7398@gmail.com>
2020-10-03 01:26:37 +0300

    edit

--- resources/views/frontend/HomePage.blade.php
+++ resources/views/frontend/HomePage.blade.php
@@ -8,36 +8,16 @@
     <link rel="stylesheet" href="{{asset('css/stylemain.css')}}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallary partisibation</title>
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav m-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('sharing.create')}}">مشاركة <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" style="direction: rtl" href="{{route('download.home')}}">13 في قلب واحد<span class="sr-only">(current)</span></a>
-
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" style="direction: rtl" href="https://www.youtube.com/watch?v=AQk3nfw6jNY&feature=youtu.be&fbclid=IwAR3Zmcsu7Rs39Q3KeiFMCZ4BUFViV5Of0-4oW4XLcu_hdF9IVRXgL_M7uNA"> رحلة ولاء تجوب الأرجاء <span class="sr-only">(current)</span></a>
-
-            </li>
-
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('sharing.show')}}">معرض المشاكات <span class="sr-only">(current)</span></a>
-            </li>
-
-
+        <ul>
+            <li><a href={{route('sharing.create')}}>مشاركة</a></li>
+            <li><a href={{route('sharing.show')}}>معرض المشاركات</a></li>
+            <li style="direction: rtl"> <a href={{route('download.home')}}>13 في قلب واحد</a></li>
+            <li style="direction: rtl"> <a href="https://www.youtube.com/watch?v=AQk3nfw6jNY&feature=youtu.be&fbclid=IwAR3Zmcsu7Rs39Q3KeiFMCZ4BUFViV5Of0-4oW4XLcu_hdF9IVRXgL_M7uNA">رحلة ولاء تجوب الأرجاء</a></li>
         </ul>
 
-    </div>
-</nav>
 <body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
